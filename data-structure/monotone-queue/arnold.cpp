e6153a1c8e27ffd05a5267f075e90e047d4deb67
Jaemin Park <arnoldpark03@gmail.com>
2024-07-22 18:36:56 +0900

    added alien, monotone

---
+++ data-structure/monotone-queue/arnold.cpp
@@ -0,0 +1,72 @@
+namespace Monotone_Queue
+{
+    // MAXN must be defined
+    const int MAXN = 5e5;
+    int N;
+
+    // dp[i] : optimal dp value of i
+    // cnt[i] : how much transitions were used in optimal dp[i]
+    // memo[i] : previous transition position from i
+    // V : restored optimal solution
+    ll dp[MAXN+10];
+    int cnt[MAXN+10], memo[MAXN+10];
+    vector<int> V;
+
+    void init(int _N)
+    {
+        N=_N;
+    }
+
+    // cost(p, q) must be monge
+    ll cost(int p, int q) {}
+
+    // Get maximum x that dp[p]+cost(p, x) < dp[q]+cost(q, x) (p is optimal than q)
+    // p < q must hold
+    int cross(int p, int q)
+    {
+        int lo=q, hi=N+1;
+        while(lo+1<hi)
+        {
+            int mid=lo+hi>>1;
+            if(dp[p]+cost(p, mid) < dp[q]+cost(q, mid)) lo=mid; // min : <, max : >
+            else hi=mid;
+        }
+        return lo;
+    }
+
+    // Get optimal dp[N]
+    // dp[i] = min(or max)_{j<i} (dp[j] + cost(j, i)) 
+    // changes dp, cnt, memo, V
+    void solve()
+    {
+        // initialize dp, cnt, memo, V, (other data structures)
+        for(int i=0; i<=N; i++) dp[i]=cnt[i]=memo[i]=0;
+        V.clear();
+
+        deque<int> DQ;
+        DQ.push_back(0);
+
+        for(int i=1; i<=N; i++)
+        {
+            while(DQ.size()>1 && dp[DQ[0]]+cost(DQ[0], i) >= dp[DQ[1]]+cost(DQ[1], i)) DQ.pop_front(); // min : >=, max : <=
+
+            // opt = argmin(or max)_{j<i} (dp[j] + cost(j, i))
+            int opt = DQ.front();
+            dp[i] = dp[opt]+cost(opt, i);
+            cnt[i] = cnt[opt]+1;
+            memo[i] = opt;
+
+            // cross(DQ[i-1], DQ[i]) < cross(DQ[i], DQ[i+1])
+            while(DQ.size()>1 && !(cross(DQ[DQ.size()-2], DQ[DQ.size()-1]) < cross(DQ[DQ.size()-1], i))) DQ.pop_back();
+            DQ.push_back(i);
+        }
+
+        for(int i=N; i>0;)
+        {
+            V.push_back(i);
+            i=memo[i];
+        }
+        V.push_back(0);
+        reverse(V.begin(), V.end());
+    }
+}
---
+++ data-structure/treap/arnold.cpp
@@ -0,0 +1,187 @@
+namespace Treap
+{
+    mt19937 rng(1557);
+    int rnd() { return uniform_int_distribution<int>()(rng); }
+
+    struct Node
+    {
+        int pr, sz;
+        int par, lc, rc;
+
+        // val : value stored in node, sum : query value of subtree
+        // lazy : lazy value to be applied to subtree of node (already applied to val, sum)
+        ll val, sum;
+        ll lazy;
+
+        Node(ll x)
+        {
+            pr=rnd();
+            sz=1;
+            par=lc=rc=0;
+
+            // your code goes here
+            val=sum=x;
+            lazy=0;
+        }
+
+        Node()
+        {
+            pr=sz=par=lc=rc=0;
+            val=sum=0;
+            lazy=0;
+        }
+    };
+
+    struct Treap
+    {
+        Treap() { NS=vector<Node>(1); root=0; }
+        int newNode(ll x) { NS.push_back(Node(x)); return NS.size()-1; }
+
+        // NS[0] : NIL node
+        int root;
+        vector<Node> NS;
+
+        // Recalculate value of node from lc, rc
+        // lazy must be empty
+        // changes sz, sum
+        void recalc(int node)
+        {
+            if(node==0) return;
+            int l=NS[node].lc, r=NS[node].rc;
+            NS[node].sz=NS[l].sz+NS[r].sz+1;
+
+            // your code goes here
+            NS[node].sum=NS[node].val+NS[l].sum+NS[r].sum;
+        }
+
+        // Apply update value upd to subtree of node
+        // update to node itself (val, sum), and to lazy value (lazy)
+        // changes lazy, val, sum
+        void apply(int node, ll upd)
+        {
+            if(node==0) return;
+
+            // your code goes here
+            NS[node].lazy+=upd;
+            NS[node].val+=upd;
+            NS[node].sum+=upd*NS[node].sz;
+        }
+
+        // Propagate lazy value to lc, rc, and initialize lazy
+        // should be called before going down to children
+        // changes lazy
+        void prop(int node)
+        {
+            if(node==0) return;
+            if(NS[node].lazy==0) return;
+            apply(NS[node].lc, NS[node].lazy);
+            apply(NS[node].rc, NS[node].lazy);
+
+            // your code goes here
+            NS[node].lazy=0;
+        }
+
+        // Merge two consecutive trees rooted at l, r
+        int merge(int l, int r)
+        {
+            if(l==0 || r==0) return l+r;
+            if(NS[l].pr>NS[r].pr)
+            {
+                prop(l);
+                int t=merge(NS[l].rc, r);
+                NS[t].par=l;
+                NS[l].rc=t;
+                recalc(l);
+                return l;
+            }
+            else
+            {
+                prop(r);
+                int t=merge(l, NS[r].lc);
+                NS[t].par=r;
+                NS[r].lc=t;
+                recalc(r);
+                return r;
+            }
+        }
+
+        // Split tree rooted at node into two trees of size k, sz-k
+        pii split(int node, int k)
+        {
+            if(node==0) return {0, 0};
+            assert(0<=k && k<=NS[node].sz);
+            prop(node);
+
+            if(k<=NS[NS[node].lc].sz)
+            {
+                auto [l, r]=split(NS[node].lc, k);
+                NS[node].lc=r;
+                NS[r].par=node;
+                NS[l].par=0;
+                recalc(node);
+                return {l, node};
+            }
+            else
+            {
+                auto [l, r]=split(NS[node].rc, k-NS[NS[node].lc].sz-1);
+                NS[node].rc=l;
+                NS[l].par=node;
+                NS[r].par=0;
+                recalc(node);
+                return {node, r};
+            }
+        }
+
+        // ================================ Essential functions ENDS here ================================
+
+        // Find kth node in subtree of node
+        int find_kth(int node, int k)
+        {
+            prop(node);
+            if(NS[NS[node].lc].sz>=k) return find_kth(NS[node].lc, k);
+            if(k==NS[NS[node].lc].sz+1) return node;
+            return find_kth(NS[node].rc, k-NS[NS[node].lc].sz-1);
+        }
+        // Find kth node of the tree
+        int find_kth(int k) { return find_kth(root, k); }
+
+        // Insert node x after the kth node of tree
+        void insert(int k, int x)
+        {
+            assert(0<=k && k<=NS[root].sz);
+            auto [a, b] = split(root, k);
+            root = merge(a, x);
+            root = merge(root, b);
+        }
+
+        // Erase kth node of tree
+        void erase(int k)
+        {
+            assert(1<=k && k<=NS[root].sz);
+            auto [a, d] = split(root, k-1);
+            auto [b, c] = split(d, 1);
+            root = merge(a, c);
+        }
+
+        // Update val to range [l, r]
+        void update(int l, int r, ll val)
+        {
+            auto [d, c] = split(root, r);
+            auto [a, b] = split(d, l-1);
+            apply(b, val);
+            root = merge(a, b);
+            root = merge(root, c);
+        }
+
+        // Query range [l, r]
+        Node query(int l, int r)
+        {
+            auto [d, c] = split(root, r);
+            auto [a, b] = split(d, l-1);
+            Node ret = NS[b];
+            root = merge(a, b);
+            root = merge(root, c);
+            return ret;
+        }
+    };
+}
