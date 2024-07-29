8082e9e3cd0fb03d5e869cd5d264f9b67f92c89b
jhwest1 <sinse0114@gmail.com>
2024-07-29 20:49:12 +0900

    modified flow/dinic/karuna.cpp

--- flow/dinic/karuna.cpp
+++ flow/dinic/karuna.cpp
@@ -3,23 +3,24 @@ using namespace std;
 
 typedef long long ll;
 
+// 0-based
 namespace dinic {
+	typedef long long T;
+	const T INF = (1ll << 63) - 1;
 	const int SZ = 101010;
 	struct edge {
-		int v, c, d;
+		int v; T c; int d;
 	};
 	vector<edge> g[SZ];
 	int n, src, snk;
 	
-	void init(int _n, int _src, int _snk) {
+	void init(int _n) {
 		for (int i = 0; i < n; i++) g[i].clear();
 		n = _n;
-		src = _src;
-		snk = _snk;
 	}
-	void add_edge(int u, int v, int c) {
+	void add_edge(int u, int v, T c, bool dir) {
 		g[u].push_back({v, c, (int)g[v].size()});
-		g[v].push_back({u, 0, (int)g[u].size() - 1});
+		g[v].push_back({u, dir ? 0 : c, (int)g[u].size() - 1});
 	}
 	int sp[SZ], ds[SZ];
 	void bfs() {
@@ -37,13 +38,13 @@ namespace dinic {
 			}
 		}
 	}
-	int dfs(int v, int f) {
+	T dfs(int v, T f) {
 		if (v == snk) return f;
 		for (; sp[v] < g[v].size(); sp[v]++) {
 			auto &e = g[v][sp[v]];
 			if (e.c == 0 || ds[e.v] != ds[v] + 1) continue;
 
-			int h = dfs(e.v, min(f, e.c));
+			T h = dfs(e.v, min(f, e.c));
 			if (h > 0) {
 				e.c -= h;
 				g[e.v][e.d].c += h;
@@ -53,15 +54,16 @@ namespace dinic {
 		return 0;
 	}
 	
-	ll flow() {
-		ll ret = 0;
+	T flow(int _src, int _snk) {
+		src = _src; snk = _snk;
+		T ret = 0;
 		while (true) {
 			for (int i = 0; i < n; i++) sp[i] = 0;
 			bfs();
 
-			ll cur = 0;
+			T cur = 0;
 			while (true) {
-				int f = dfs(src, (1 << 31) - 1);
+				int f = dfs(src, INF);
 				if (f > 0) cur += f;
 				else break;
 			}
@@ -72,16 +74,16 @@ namespace dinic {
 	}
 };
 
-// LOJ 127
+// https://loj.ac/p/127
 int main() {
 	cin.tie(0); ios_base::sync_with_stdio(0);
 	int n, m, s, t;
 	cin >> n >> m >> s >> t;
-	dinic::init(n, s - 1, t - 1);
+	dinic::init(n);
 	for (int i = 0; i < m; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
-		dinic::add_edge(u - 1, v - 1, w);
+		dinic::add_edge(u - 1, v - 1, w, true);
 	}
-	cout << dinic::flow() << '\n';
+	cout << dinic::flow(s - 1, t - 1) << '\n';
 }
