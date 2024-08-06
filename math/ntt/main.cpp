b6965063acb9017a39b1d0d173e453253dd28be7
YeongTree <blackking26@naver.com>
2024-08-06 17:12:13 +0900

    Update main.cpp

--- math/ntt/main.cpp
+++ math/ntt/main.cpp
@@ -2,36 +2,23 @@ const int MOD = 998244353;
 const mint G = 3; // primitive root
 // or (MOD, G) = (0xffffffff00000001, 7)
 // define mpow here
-int M, B;
-vector<int> rev;
-vector<mint> w;
-void init(int _B) {
-	B = _B;
-	M = 1 << B;
-	w.resize(2 * M + 1);
-	rev.resize(M);
-	mint g = mpow(G, (MOD - 1) >> (B + 1));
-	w[0] = 1;
-	for (int i = 1; i <= 2 * M; i++) w[i] = w[i - 1] * g;
-	for (int i = 0; i < M; i++) {
-		rev[i] = (rev[i / 2] >> 1) | ((i & 1) << (B - 1));
-	}
-}
 void dft(vector<mint> &F, bool inv) {
 	int n = F.size();
 	int b = __lg(n);
+	vector<int> rev(n);
 	for (int i = 0; i < n; i++) {
-		int p = rev[i] >> (B - b);
-		if (i < p) swap(F[i], F[p]);
+		rev[i] = (rev[i / 2] >> 1) | ((i & 1) << (b - 1));
+		if(i < rev[i]) swap(F[i], F[rev[i]]);
 	}
 	for (int d = 1; d < n; d <<= 1) {
+		mint w = mpow(G, inv ? MOD - 1 - (MOD - 1) / (2 * d) : (MOD - 1) / (2 * d));
 		for (int i = 0; i < n; i += 2 * d) {
+			mint r = 1;
 			for (int j = 0; j < d; j++) {
-				mint r = inv ? w[2 * M - M / d * j] : w[M / d * j];
-				mint a = F[i + j];
-				mint b = F[i + j + d] * r;
-				F[i + j] = a + b;
-				F[i + j + d] = a - b;
+				mint b = F[i|j|d] * r;
+				F[i|j|d] = F[i|j] - b;
+				F[i|j] += b;
+				r *= w;
 			}
 		}
 	}
