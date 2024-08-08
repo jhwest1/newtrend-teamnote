1c30e14622bef12d0c07b7d8ae0edda59fc87661
YeongTree <blackking26@naver.com>
2024-08-08 14:04:29 +0900

    Update main.cpp

--- math/ntt/main.cpp
+++ math/ntt/main.cpp
@@ -10,15 +10,18 @@ void dft(vector<mint> &F, bool inv) {
 		rev[i] = (rev[i / 2] >> 1) | ((i & 1) << (b - 1));
 		if(i < rev[i]) swap(F[i], F[rev[i]]);
 	}
+	vector<mint> w(n);
+	w[0] = w[1] = 1;
+	for (int k = 2; k < n; k <<= 1) {
+		mint z[2] = {1, mpow(G, inv ? MOD - 1 - (MOD - 1) / k / 2 : (MOD - 1) / k / 2)};
+		for(int i = k; i < (k << 1); ++i) w[i] = w[i >> 1] * z[i & 1];
+	}
 	for (int d = 1; d < n; d <<= 1) {
-		mint w = mpow(G, inv ? MOD - 1 - (MOD - 1) / (2 * d) : (MOD - 1) / (2 * d));
 		for (int i = 0; i < n; i += 2 * d) {
-			mint r = 1;
 			for (int j = 0; j < d; j++) {
-				mint b = F[i|j|d] * r;
+				mint b = F[i|j|d] * w[j|d];
 				F[i|j|d] = F[i|j] - b;
 				F[i|j] += b;
-				r *= w;
 			}
 		}
 	}
@@ -28,12 +31,13 @@ void dft(vector<mint> &F, bool inv) {
 	}
 }
 vector<mint> multiply(vector<mint> F, vector<mint> G) {
-	int n = 1, m = (int)F.size() + (int)G.size() - 1;
-	while (n <= m) n <<= 1;
+	int n = 1;
+    int sz=F.size()+G.size()-1;
+	while (n < (int)F.size() + G.size()) n <<= 1;
 	F.resize(n); G.resize(n);
 	dft(F, false); dft(G, false);
 	for (int i = 0; i < n; i++) F[i] *= G[i];
 	dft(F, true);
-	F.resize(m);
+	F.resize(sz);
 	return F;
 }
