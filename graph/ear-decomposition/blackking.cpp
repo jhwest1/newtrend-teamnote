3ae0de1997c5175f8ef40c6483b985d8e9d8c334
YeongTree <blackking26@naver.com>
2024-08-17 15:58:10 +0900

    .

--- graph/ear-decomposition/blackking.cpp
+++ graph/ear-decomposition/blackking.cpp
@@ -1,31 +1,231 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define piii pair<int, pii>
+#define plll pair<long long, pll>
+#define tiii array<int, 3>
+#define tiiii array<int, 4>
+#define ff first
+#define ss second
+#define ee ss.ff
+#define rr ss.ss
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+
 // no loop, 2-vertex-connected: only first ear is cycle, 2-edge-connected: every ear can be cycle.
+// call init() first, add_edge(x, y), then ear_decomposition()
+// vear: vertex list of ear / ear : edge list of ear
+// path: vear = {1, 2, 3}, ear = {{1, 2}, {2, 3}}
+// cycle: vear = {1, 2, 3, 1}, ear = {{1, 2}, {2, 3}, {3, 1}}
 const int SV = 101010;
-int n, cnt = 0; vector<int> gph[SV], ls[SV]; bool chc[SV]; int ord[SV], rev[SV], par[SV]; vector<vector<int>> ear;
-void init() {
-	for(int i = 0; i <= n; ++i) gph[i].clear(), chc[i] = false, ord[i] = 0, rev[i] = 0, par[i] = 0, ls[i].clear();
-	ear.clear();
+int n, cnt = 0, ord[SV], rev[SV]; bool chc[SV]; pii par[SV]; vector<pii> gph[SV]; vector<vector<int>> vear, ear;
+void init(int _n) {
+	n = _n;
+	for(int i = 0; i <= n; ++i) gph[i].clear(), chc[i] = false, ord[i] = 0, rev[i] = 0, par[i] = {0, 0};
+	vear.clear(); ear.clear();
 	cnt = 0;
 }
-void dfs(int x, int p) {
-	par[x] = p;
+void add_edge(int x, int y, int e) {
+	gph[x].push_back({y, e});
+	gph[y].push_back({x, e});
+}
+void dfs(int x, int p, int pe) {
+	par[x] = {p, pe};
 	ord[x] = ++cnt;
-	for(auto y : gph[x]) if(!ord[y]) dfs(y, x);
+	for(auto [y, e] : gph[x]) if(!ord[y]) dfs(y, x, e);
 }
 void ear_decomposition() {
-	dfs(1, 1); // 1-based
-	for(int i = 0; i < n; ++i) rev[ord[i]] = i;
+	dfs(1, 0, 0); // 1-based
+	for(int i = 1; i <= n; ++i) rev[ord[i]] = i;
 	for(int i = 1; i <= n; ++i) {
 		int x = rev[i];
-		for(auto y : gph[x]) if(ord[y] > ord[x] && par[y] != x) {
-			vector<int> path{x};
+		for(auto [y, e] : gph[x]) if(ord[y] > ord[x] && par[y].ff != x) {
+			vector<int> V{x, y};
+			vector<int> E{e};
 			chc[x] = true;
 			while(!chc[y]) {
 				chc[y] = true;
-				path.push_back(y);
-				y = par[y];
+				V.push_back(par[y].ff);
+				E.push_back(par[y].ss);
+				y = par[y].ff;
 			}
-			path.push_back(y);
-			ear.push_back(path);
+			vear.push_back(V);
+			ear.push_back(E);
+		}
+	}
+}
+
+
+// checker
+
+mt19937 rnd(1557);
+int rng(int l, int r) { return uniform_int_distribution<int>(l, r)(rnd); }
+map<pii, int> M;
+void gen_gph(int m)
+{
+	M.clear();
+	for(int i = 1; i <= m; ++i)
+	{
+		int x = rng(1, n);
+		int y = rng(1, n);
+		if(x == y || M.count({x, y})) {--i; continue;}
+		add_edge(x, y, i);
+		M[{x, y}] = i;
+		M[{y, x}] = i;
+	}
+}
+
+// int ncnt = 0;
+// int nord[SV];
+// bool flag = true;
+// int ndfs(int x, int p)
+// {
+// 	int ret = nord[x] = ++ncnt;
+// 	int ch = 0;
+// 	for(auto [y, e] : gph[x])
+// 	{
+// 		if(!nord[y])
+// 		{
+// 			int t = ndfs(y, x);
+// 			ret = min(ret, t);
+// 			if(p != -1 && t == nord[x]) flag = false;
+// 			++ch;
+// 		}
+// 		else ret = min(ret, nord[y]);
+// 	}
+// 	if(p == -1 && ch > 1) flag = false;
+// 	return ret;
+// }
+
+// bool _2_con_check()
+// {
+// 	flag = true;
+// 	ncnt = 0;
+// 	for(int i = 1; i <= n; ++i) nord[i] = false;
+// 	ndfs(1, -1);
+
+// 	if(!flag) return false;
+// 	for(int i = 1; i <= n; ++i) if(!nord[i]) return false;
+// 	return true;
+// }
+
+// bool used[SV];
+// bool check()
+// {
+// 	for(int i = 1; i <= n; ++i) used[i] = false;
+// 	bool flag = false;
+// 	for(int i = 0; i < (int)vear.size(); ++i)
+// 	{
+// 		auto &v = vear[i], &e = ear[i];
+// 		if(flag && (!used[v.front()] || !used[v.back()])) return false;
+// 		if(flag && v.front() == v.back()) return false;
+// 		flag = true;
+// 		for(int i = 0; i < (int)v.size(); ++i)
+// 		{
+// 			if(i != 0 && i != (int)v.size() - 1 && used[v[i]]) return false;
+// 			used[v[i]] = true;
+// 			if(i != 0)
+// 			{
+// 				if(M[{v[i - 1], v[i]}] != e[i - 1]) return false;
+// 				M[{v[i - 1], v[i]}] = M[{v[i], v[i - 1]}] = -1;
+// 			}
+// 		}
+// 	}
+// 	return true;
+// }
+
+int ncnt = 0;
+int nord[SV];
+bool flag = true;
+int ndfs(int x, int p)
+{
+	int ret = nord[x] = ++ncnt;
+	for(auto [y, e] : gph[x])
+	{
+		if(!nord[y])
+		{
+			int t = ndfs(y, x);
+			ret = min(ret, t);
+		}
+		else ret = min(ret, nord[y]);
+	}
+	if(p != -1 && ret == nord[x]) flag = false;
+	return ret;
+}
+
+bool _2_con_check()
+{
+	flag = true;
+	ncnt = 0;
+	for(int i = 1; i <= n; ++i) nord[i] = false;
+	ndfs(1, -1);
+
+	if(!flag) return false;
+	for(int i = 1; i <= n; ++i) if(!nord[i]) return false;
+	return true;
+}
+
+bool used[SV];
+bool check()
+{
+	for(int i = 1; i <= n; ++i) used[i] = false;
+	bool flag = false;
+	for(int i = 0; i < (int)vear.size(); ++i)
+	{
+		auto &v = vear[i], &e = ear[i];
+		if(flag && (!used[v.front()] || !used[v.back()])) return false;
+		flag = true;
+		for(int i = 0; i < (int)v.size(); ++i)
+		{
+			if(i != 0 && i != (int)v.size() - 1 && used[v[i]]) return false;
+			used[v[i]] = true;
+			if(i != 0)
+			{
+				if(M[{v[i - 1], v[i]}] != e[i - 1]) return false;
+				M[{v[i - 1], v[i]}] = M[{v[i], v[i - 1]}] = -1;
+			}
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	n = 10000;
+	int c = 0;
+	while(c <= 1'000'000)
+	{
+		init(n);
+		gen_gph(c += 1000);
+		if(_2_con_check())
+		{
+			ear_decomposition();
+			cout << "c = " << c << ": ";
+			if(check())
+			{
+				cout << "correct" << endl;
+			}
+			else
+			{
+				cout << "error!" << endl;
+				cout << "graph:" << endl;
+				pii edge[c + 1];
+				for(auto [x, y] : M) edge[y] = x;
+				for(int i = 1; i <= c; ++i) cout << edge[i].ff << ' ' << edge[i].ss << endl;
+				cout << "output:" << endl;
+				for(int i = 0; i < (int)vear.size(); ++i)
+				{
+					cout << "("; for(auto j : vear[i]) cout << j << ' '; cout << ") ";
+					cout << "("; for(auto j : ear[i]) cout << j << ' '; cout << ")" << endl;
+					cout << endl;
+				}
+				return 0;
+			}
 		}
 	}
 }
