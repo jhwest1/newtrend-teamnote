5952a842ed3da1e7b1f046e442375aca310664de
arnold518 <arnoldpark03@gmail.com>
2024-09-07 20:17:15 +0900

    bitset-lcs

--- string/bitset-lcs/main.cpp
+++ string/bitset-lcs/main.cpp
@@ -1,48 +1,39 @@
-//O(NM/64)
-
+// O(NM/64)
 #define get(arr, x) (((arr[x >> 6] >> (x & 63)) & 1) == 1)
 #define set(arr, x) (arr[x >> 6] |= 1llu << (x & 63))
-using ulint = unsigned long long;
-
+using ull = unsigned long long;
 // return lcs
-int lcs(std::string A, std::string B) {
+int lcs(string A, string B) {
   int N = A.size(), M = B.size();
   int sz = (M >> 6) + 1;
 
-  std::vector<ulint> S[256];
+  vector<ull> S[256];
   for(int c = 0; c < 256; c++) S[c].resize(sz);
-  for(int j = 0; j < M; j++) set(S[B[j]], j);
-  
-  std::vector<ulint> row(sz);
+  for(int j = 0; j < M; j++) set(S[B[j]], j);  
+  vector<ull> row(sz);
   for(int j = 0; j < M; j++) if(A[0] == B[j]) { set(row, j); break; }
 
   for(int i = 1; i < N; i++) {
-    ulint shl_carry = 1;
-    ulint minus_carry = 0;
-    
+    ull shl_carry = 1;
+    ull minus_carry = 0;
     for(int k = 0; k < sz; k++) {
       // Compute k-th block of x == S[A[i]] OR D[i-1]
-      ulint x_k = S[A[i]][k] | row[k];
-
+      ull x_k = S[A[i]][k] | row[k];
       // Compute k-th block of "(D[i-1] << 1) | 1"
-      ulint term_1 = (row[k] << 1) | shl_carry;
+      ull term_1 = (row[k] << 1) | shl_carry;
       shl_carry = row[k] >> 63;
-
       // Compute k-th block of "x - ((D[i-1] << 1) | 1)"
-      auto sub_carry = [](ulint &x, ulint y){
-        ulint tmp = x;
+      auto sub_carry = [](ull &x, ull y){
+        ull tmp = x;
         return (x = tmp - y) > tmp;
       };
-      ulint term_2 = x_k;
+      ull term_2 = x_k;
       minus_carry = sub_carry(term_2, minus_carry);
       minus_carry += sub_carry(term_2, term_1);
-      
       row[k] = x_k & (x_k ^ term_2);
     }
-
     row[M >> 6] &= (1llu << (M & 63)) - 1;
   }
-
   int ret = 0;
   for(int j = 0; j < M; j++) if(get(row, j)) ret += 1;
   return ret;
---
+++ string/bitset-lcs/test.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// O(NM/64)
+#define get(arr, x) (((arr[x >> 6] >> (x & 63)) & 1) == 1)
+#define set(arr, x) (arr[x >> 6] |= 1llu << (x & 63))
+using ull = unsigned long long;
+// return lcs
+int lcs(string A, string B) {
+  int N = A.size(), M = B.size();
+  int sz = (M >> 6) + 1;
+
+  vector<ull> S[256];
+  for(int c = 0; c < 256; c++) S[c].resize(sz);
+  for(int j = 0; j < M; j++) set(S[B[j]], j);
+  
+  vector<ull> row(sz);
+  for(int j = 0; j < M; j++) if(A[0] == B[j]) { set(row, j); break; }
+
+  for(int i = 1; i < N; i++) {
+    ull shl_carry = 1;
+    ull minus_carry = 0;
+    for(int k = 0; k < sz; k++) {
+      // Compute k-th block of x == S[A[i]] OR D[i-1]
+      ull x_k = S[A[i]][k] | row[k];
+      // Compute k-th block of "(D[i-1] << 1) | 1"
+      ull term_1 = (row[k] << 1) | shl_carry;
+      shl_carry = row[k] >> 63;
+      // Compute k-th block of "x - ((D[i-1] << 1) | 1)"
+      auto sub_carry = [](ull &x, ull y){
+        ull tmp = x;
+        return (x = tmp - y) > tmp;
+      };
+      ull term_2 = x_k;
+      minus_carry = sub_carry(term_2, minus_carry);
+      minus_carry += sub_carry(term_2, term_1);
+      row[k] = x_k & (x_k ^ term_2);
+    }
+    row[M >> 6] &= (1llu << (M & 63)) - 1;
+  }
+  int ret = 0;
+  for(int j = 0; j < M; j++) if(get(row, j)) ret += 1;
+  return ret;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    string S, T;
+    cin >> S >> T;
+    cout << lcs(S, T);
+}
